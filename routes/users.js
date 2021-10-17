892a4cfc646ac687f0251cb6d98dabf77157feb8
JonathanEmanuel12 <jonathaanemanuel21@gmail.com>
2021-10-17 09:21:02 -0300

    Criação de rotas

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,20 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', async (req, res, next) => {
   res.json({users: [{name: 'Timmy'}]});
 });
+
+router.post('/', async (req, res) => {
+  res.json({usuarios: "Salvos"});
+});
+
+router.put('/:id', async (req, res) => {
+  res.json({usuarios: "Alterado " + req.params.id});
+});
+
+router.delete('/:id', async (req, res) => {
+  res.json({usuarios: "Deletado " + req.params.id});
+})
 
 module.exports = router;
